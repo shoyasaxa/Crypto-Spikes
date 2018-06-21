9dd1f50b82efad486e0c0b74e6c532afcdc9ffdc
Shoya Yoshida <sy519@georgetown.edu>
2018-06-21 17:03:51 -0400

    ran tpot for 4 hours

--- tpot_crypto_spike_predictor.py
+++ tpot_crypto_spike_predictor.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_selection import SelectPercentile, f_classif
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import make_pipeline, make_union
 from sklearn.preprocessing import Imputer
-from sklearn.tree import DecisionTreeClassifier
+from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -17,10 +18,10 @@ imputer.fit(training_features)
 training_features = imputer.transform(training_features)
 testing_features = imputer.transform(testing_features)
 
-# Score on the training set was:0.8797150997150996
+# Score on the training set was:0.8802849002849003
 exported_pipeline = make_pipeline(
-    SelectPercentile(score_func=f_classif, percentile=1),
-    DecisionTreeClassifier(criterion="gini", max_depth=6, min_samples_leaf=20, min_samples_split=13)
+    StackingEstimator(estimator=RandomForestClassifier(bootstrap=False, criterion="entropy", max_features=1.0, min_samples_leaf=9, min_samples_split=17, n_estimators=100)),
+    LogisticRegression(C=0.0001, dual=False, penalty="l1")
 )
 
 exported_pipeline.fit(training_features, training_target)
